b837d7a277f4879252871cdb9f1eb9d2917ec3d0
petermwesigwa <mps1401@gmail.com>
2019-11-12 15:37:08 -0500

    update dev from master

--- routes.py
+++ routes.py
@@ -3,8 +3,26 @@ from database import User
 from flask import render_template
 
 @app.route("/")
+@app.route("/index")
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+
+@app.route("/homecustomer")
+def homecustomer():
+    return render_template('homecustomer.html')
+
+@app.route("/homedeliver")
+def homedeliver():
+    return render_template('homedeliver.html')
+
+@app.route("/deliveries")
+def deliveries():
+    return render_template('deliveries.html')
+
+@app.route("/cart")
+def cart():
+    return render_template('cart.html')
+
 
 @app.route("/about")
 def about():
