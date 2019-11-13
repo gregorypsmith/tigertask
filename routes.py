d59aa001831fc621ca687c48c1a6d7702fbe69d3
petermwesigwa <mps1401@gmail.com>
2019-11-12 22:23:47 -0500

    fix if query is none

--- routes.py
+++ routes.py
@@ -10,6 +10,8 @@ def home():
 @app.route("/homecustomer")
 def homecustomer():
     query = request.args.get('query')
+    if query is None:
+        query = ""
 
     items = Item.query.filter(Item.name.contains(query))
 
