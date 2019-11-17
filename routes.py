a75d08ada08ea9e0ecb885b80e6c924c047e9131
Gregory Smith <gpsmith@princeton.edu>
2019-11-17 14:02:09 -0500

    added cas

--- routes.py
+++ routes.py
@@ -1,14 +1,20 @@
 from app import app, db, mail
 from database import Customer, Deliverer, CartItem, Order, OrderItem, Item
 from flask import render_template, request, make_response
+from CASClient import  CASClient
 
 @app.route("/")
 @app.route("/index")
 def home():
+
+    username = CASClient().authenticate()
     return render_template('index.html')
 
 @app.route("/homecustomer")
 def homecustomer():
+
+    username = CASClient().authenticate()
+
     query = request.args.get('query')
 
     if query is None:
@@ -32,17 +38,21 @@ def homecustomer():
 
 @app.route("/homedeliver")
 def homedeliver():
+    username = CASClient().authenticate()
     return render_template('homedeliver.html')
 
 @app.route("/deliveries")
 def deliveries():
+    username = CASClient().authenticate()
     return render_template('deliveries.html')
 
 @app.route("/cart")
 def cart():
+    username = CASClient().authenticate()
     return render_template('cart.html')
 
 
 @app.route("/about")
 def about():
+    username = CASClient().authenticate()
     return render_template('about.html')
