5f9950f89385daa37655e1d45e74406be2ea1909
Gregory Smith <gpsmith@princeton.edu>
2019-11-17 22:56:00 -0500

    fix for heroku

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from flask_mail import Mail
 from utils import is_heroku
 
 app = Flask(__name__)
+import routes
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 admin = Admin(app)
