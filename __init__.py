334d15b634744d3703003545dee57e0c560fa59e
Gregory Smith <gpsmith@princeton.edu>
2020-05-29 11:31:13 -0400

    update db

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@ import os
 
 from routes import app
 import routes
-from database import Customer, Deliverer, Item, Order, CartItem, OrderItem, db
 
 if __name__ == "__main__":
     app.run(debug=True)
